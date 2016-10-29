e0259381ba7c414acc498cc1d160bc69cb101c5e
William Howard <wipatric@umich.edu>
2016-10-29 17:51:13 -0400

    implemented the gen_sets function. No testing.

--- poise.cpp
+++ poise.cpp
@@ -37,6 +37,9 @@ int main()
 	read_store(&head, "Head.txt");
 	read_store(&hand, "Hand.txt");
 	read_store(&leg, "Leg.txt");
+	
+	vector <set> sets;
+	gen_sets(&chest, &head, &hand, &leg, &sets);
 }
 
 void read_store(vector<armor>* output, string input)
@@ -55,12 +58,32 @@ void read_store(vector<armor>* output, string input)
 
 void gen_sets(vector<armor>* chest, vector<armor>* head, vector<armor>* hand, vector<armor>* leg, vector<set>* sets)
 {
-	return;
+	cout << "reading set permutations" << endl;
+	for(int i = 0; i<head->size(); i++)
+	{
+		for(int j = 0; j<chest->size(); j++)
+		{
+			for(int k = 0; k<hand->size(); k++)
+			{
+				for(int l = 0; l<leg->size(); l++)
+				{
+					set temp;
+					temp.head = &head->at(i);
+					temp.chest = &chest->at(j);
+					temp.hand = &hand->at(k);
+					temp.leg = &leg->at(l);
+					temp.weight_total = temp.head->weight + temp.chest->weight + temp.hand->weight + temp.leg->weight;
+					temp.poise_total = temp.head->poise + temp.chest->poise + temp.hand->poise + temp.leg->poise;
+					sets->push_back(temp);
+				}
+			}
+		}
+	}
 }
 
 void print_armor(armor* input)
 {
-		cout << "w=" << input->weight << " p=" << input->poise << endl;
+	cout << "w=" << input->weight << " p=" << input->poise << endl;
 }
 
 void print_set(set* input)
