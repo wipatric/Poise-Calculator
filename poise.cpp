ea3bf570110a5de1fef0d7eb88d17475b38aa1ca
William Howard <wipatric@umich.edu>
2016-10-29 17:01:46 -0400

    this is the initial commit

---
+++ poise.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+struct armor
+{
+	double weight;
+	double poise;
+};
+
+void read_store(vector<armor>* output, string input);
+void print_armor(vector<armor>* input);
+
+int main()
+{
+
+	vector<armor> chest;
+	vector<armor> head;
+	vector<armor> hand;
+	vector<armor> leg;
+	read_store(&chest, "Chest.txt");
+	read_store(&head, "Head.txt");
+	read_store(&hand, "Hand.txt");
+	read_store(&leg, "Leg.txt");
+	print_armor(&chest);
+}
+
+void read_store(vector<armor>* output, string input)
+{
+	ifstream infile;
+	infile.open(input.c_str());
+	cout << "reading: " << input << endl;
+	while(!infile.eof())
+	{
+		armor chestpiece;
+		infile >> chestpiece.weight >> chestpiece.poise;
+		output->push_back(chestpiece);
+	}
+	
+}
+
+void print_armor(vector<armor>* input)
+{
+	for(int i=0; i<input->size(); i++)
+	{
+		cout << "w=" << input->at(i).weight << " p=" << input->at(i).poise << endl;
+	}
+	return;
+}
